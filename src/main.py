226788f6f388ec1b7f4e0bd6613493fa3f53bec2
BetoVT <100742695+BetoVT@users.noreply.github.com>
2023-09-25 16:21:04 -0600

    test_api

--- src/main.py
+++ src/main.py
@@ -7,3 +7,33 @@ app = FastAPI()
 @app.get("/inicio")
 async def ruta_prueba():
     return "Hola"
+
+@app.get("/inicio2")
+async def ruta_prueba2():
+    return "Adios"
+
+@app.get("/PlayTimeGenre")
+def playTimeGenre(genre):
+    year = 0
+    return year
+
+@app.get("/UserForGenre")
+def userForGenre(genre):
+    user = 1.1
+    yearList = 1.2
+    return user, yearList
+
+@app.get("/UsersRecommend")
+def usersRecommend(year):
+    gameList = 2
+    return gameList
+
+@app.get("/UsersNotRecommend")
+def usersNotRecommend(genre):
+    gameList = 3
+    return gameList
+
+@app.get("/SentimentAnalysis")
+def sentimentAnalysis(entName):
+    reviews = 4
+    return reviews

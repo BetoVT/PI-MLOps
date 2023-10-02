3af40ef696150bc858e1c8b4d37695654f6a96e0
BetoVT <albertoars141@gmail.com>
2023-10-02 11:44:02 -0600

    laptop-commit4

--- src/process/process_users_items.py
+++ src/process/process_users_items.py
@@ -9,48 +9,55 @@ PARAMS_ITEMS = ["{\'item_id': \'", "\', \'item_name\': \'",
 PARAMS_ITEMS_ORIGINAL = ["\', 'playtime_forever': ",
                          ", \'playtime_2weeks\': ", "}, {", "}]"]
 
-def search_item(s, i):
-        
+def fixStr_user_reviews(line):
+    a = PARAMS_ITEMS_ORIGINAL
+    b = PARAMS_ITEMS
+    for i in range(len(a)):
+        line = line.replace(a[i], b[i + 2].replace("\'", "\""))
+    for i in range(len(b)):
+        line = line.replace(b[i], b[i].replace("\'", "\""))
+    return line
+
+def search_item(s, past_length):
+    str_strt = s.find("{\"item_id\": \"", past_length)
+    str_end = s.find("\"}", past_length) + 2
+    s = s[str_strt:str_end]
+    #print(s)
     return s
 
-def count_items(s, count):
+def add_items(s, count):
+    s = fixStr_user_reviews(s)
     lst = []
+    past_length = 0
     for i in range(count):
-        lst.append(search_item(s, i))
+        new_item = search_item(s, past_length)
+        lst.append(json.loads(new_item))
+        #print("Saved length: ", past_length)
+        past_length = past_length + len(new_item) + 2
     return lst
+
+#print(add_items(test, test_count))
 
 def untangle_df(df, foreign_key):
     simplefilter(action="ignore", category=pd.errors.PerformanceWarning)
+    items = []
+    for i in range(df.shape[0]):
+        s = df.at[i, 'items']
+        count = df.at[i, 'items_count']
+        #print("count is: ", count)
+        #print("line is: ", i + 1)
+        #print("string is: ", s)
+        items = items + add_items(s, count)
     
-    a = PARAMS_ITEMS_ORIGINAL
-    b = PARAMS_ITEMS
-    parameters = []
-    for i in b:
-        parameters.append(i.replace("\'", "\""))
-    
-    df['item_amount'] = df.apply(lambda row : count_items(row['items']), axis=1)
-
-
-    #for i in df[foreign_key]:
-    #    i = (i.replace(a[0], b[2])
-    #          .replace(a[1], b[3])
-    #          .replace(a[2], b[4])
-    #          .replace(a[3], b[5]))
-    #    #temp = json.loads(i)
-    #    t.append(i)
-    return df
+    return items
 
 def untangle_csv(primary_key, foreign_key):
     df = pd.read_csv('data\\processed\\users_items.csv',
                      encoding="UTF-8", index_col=0)
     df.drop_duplicates(inplace=True)
-    df = untangle_df(df[[primary_key, foreign_key, 'items_count']].copy(), foreign_key)
-    print(df)
-    print(df['item_amount'].value_counts())
-    print(df['item_amount'].sum())
-    print(df['items_count'].value_counts())
-    print(df['items_count'].sum())
-    #print(df)
-    #df.to_csv('data\\processed\\steam_games_' + foreign_key + '.csv')
+    df.reset_index(drop=True, inplace=True)
+    df2 = pd.DataFrame.from_dict(untangle_df(df[[primary_key, foreign_key, 'items_count']].copy(), foreign_key))
+    print(df2)
+    df2.to_csv('data\\processed\\user_' + foreign_key + '_flat.csv')
 
 untangle_csv('user_id', 'items')
